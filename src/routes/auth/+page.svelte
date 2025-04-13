0a8f0ee50d63ca445ecc3727ef034330cd915e29
NetSMG <150570330+netsmg@users.noreply.github.com>
2025-04-13 13:32:51 +0600

    Create +page.svelte

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+	import PageTitle from '$lib/components/PageTitle.svelte';
+	import AuthForm from '$lib/sections/AuthForm.svelte';
+</script>
+
+<svelte:head>
+	<title>Auth | Amar Shop</title>
+	<meta name="description" content="Login or Register" />
+</svelte:head>
+
+<PageTitle pageTitle="Auth" />
+
+<section class="contact">
+	<div class="contact__title">
+		<p>Create an account or login to your existing account.</p>
+	</div>
+	<AuthForm />
+</section>
+
+<style>
+	.contact {
+		padding: var(--space-l) var(--space-m);
+	}
+
+	.contact__title {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		gap: var(--space-xl);
+		justify-content: start;
+		align-items: center;
+		margin-bottom: var(--space-xl);
+	}
+
+	.contact__title h2 {
+		font-size: var(--step-5);
+		font-family: var(--font-fancy);
+
+		line-height: var(--leading-none);
+	}
+
+	.contact__title p {
+		display: block;
+		max-width: 60ch;
+	}
+</style>
