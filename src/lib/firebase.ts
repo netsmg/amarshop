ae2c82d8c361765775bc1478f0dbfdaea3ac4c08
NetSMG <150570330+netsmg@users.noreply.github.com>
2025-04-13 16:54:39 +0600

    Create firebase.ts

---
+++ src/lib/firebase.ts
@@ -0,0 +1,21 @@
+// src/lib/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDZC-buVhG9GkefVodM9Ku-jeD_kxdOBCM",
+  authDomain: "webuinet.firebaseapp.com",
+  databaseURL: "https://webuinet-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "webuinet",
+  storageBucket: "webuinet.appspot.com",
+  messagingSenderId: "591886980663",
+  appId: "1:591886980663:web:51eb2a39cb869d792711b2",
+  measurementId: "G-NEE9ZLKHHE"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
