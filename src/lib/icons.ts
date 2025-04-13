2b93405ae7249285f48fbd73d4f2addfb53f3184
NetSMG <150570330+netsmg@users.noreply.github.com>
2025-04-13 15:35:19 +0600

    Update icons.ts

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -1,15 +1,23 @@
 import ArrowRight from '~icons/iconoir/arrow-right';
-
 import ArrowLeft from '~icons/iconoir/arrow-left';
-
 import Menu from '~icons/iconoir/menu';
-
 import Xmark from '~icons/iconoir/xmark';
-
 import SendMail from '~icons/iconoir/send-mail';
-
 import SendDiagonal from '~icons/iconoir/send-diagonal';
-
 import Eth from '~icons/iconoir/ethereum-circle';
+import Checkmark from '~icons/iconoir/check';
+import Cross from '~icons/iconoir/cancel';
+import Lock from '~icons/iconoir/lock';
 
-export { ArrowRight, Menu, Xmark, SendMail, ArrowLeft, SendDiagonal, Eth };
+export {
+  ArrowRight,
+  Menu,
+  Xmark,
+  SendMail,
+  ArrowLeft,
+  SendDiagonal,
+  Eth,
+  Checkmark,
+  Cross,
+  Lock,
+};
