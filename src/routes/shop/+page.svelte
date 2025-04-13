30f50a38e63c21065b49d1518a0e25138c7ebbe6
NetSMG <150570330+netsmg@users.noreply.github.com>
2025-04-13 15:52:47 +0600

    Create +page.svelte

---
+++ src/routes/shop/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import PageTitle from '$lib/components/PageTitle.svelte';
+	import McqCard from '$lib/components/mcq/McqCard.svelte';
+</script>
+
+<svelte:head>
+	<title>Shop| Amar Shop</title>
+	<meta name="description" content="Give mcq" />
+</svelte:head>
+
+<PageTitle pageTitle="Shop" />
+
+<section class="contact">
+	
+	<McqCard />
+</section>
+
+<style>
+	.contact {
+		padding: var(--space-l) var(--space-m);
+	}
+</style>
