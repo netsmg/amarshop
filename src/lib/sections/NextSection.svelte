21d3768fddb8db3543204adbc0f724674aed1ccd
NetSMG <150570330+netsmg@users.noreply.github.com>
2025-04-18 18:32:29 +0600

    Update NextSection.svelte

--- src/lib/sections/NextSection.svelte
+++ src/lib/sections/NextSection.svelte
@@ -306,11 +306,9 @@
         transform: translateY(-5px);
     }
 
-    
-.feature-icon {
+    .feature-icon {
     font-size: 2.5rem;
     margin-bottom: 1rem;
-    
     display: flex;
     justify-content: center;
     align-items: center;
