971f3606078c285d47406d8bad0f0bbe74c463f7
NetSMG <150570330+netsmg@users.noreply.github.com>
2025-05-02 10:27:34 +0600

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,21 +2,22 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
-// firebaseConfig.js
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD-DemoKeyHere1234567890abcdefg",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef123456",
-  measurementId: "G-ABCDEF1234"
+  apiKey: "AIzaSyDZC-buVhG9GkefVodM9Ku-jeD_kxdOBCM",
+  authDomain: "webuinet.firebaseapp.com",
+  databaseURL: "https://webuinet-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "webuinet",
+  storageBucket: "webuinet.appspot.com",
+  messagingSenderId: "591886980663",
+  appId: "1:591886980663:web:51eb2a39cb869d792711b2",
+  measurementId: "G-NEE9ZLKHHE"
 };
 
-export default firebaseConfig;
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage= getStorage(app);
