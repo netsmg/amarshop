3fe31369c3548c7643f218778f5bdd38c9ee89e5
NetSMG <150570330+netsmg@users.noreply.github.com>
2025-05-04 13:09:43 +0600

    Update hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,11 @@
 // src/hooks.server.ts
 export async function handle({ event, resolve }) {
 	const response = await resolve(event);
-	response.headers.set('Access-Control-Allow-Origin', '*'); // Or set your domain instead of *
-	response.headers.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+
+	// Add CORS headers to the response
+	response.headers.set('Access-Control-Allow-Origin', '*');
+	response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+	response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Accept');
+
 	return response;
 }
